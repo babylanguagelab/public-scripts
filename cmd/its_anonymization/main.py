b431e78168c3a4982b66f6d7a2cab2164b385100
unioah <unioah@gmail.com>
2015-09-03 12:42:27 -0500

    update to generate spreadsheet

--- cmd/its_anonymization/main.py
+++ cmd/its_anonymization/main.py
@@ -4,6 +4,7 @@ import xml.etree.cElementTree as ET
 import time
 import datetime
 from dateutil.relativedelta import relativedelta
+import csv
 
 
 # default configurations for domain items
@@ -80,6 +81,14 @@ class XMLParser2:
     def save(self, filename):
         self.tree.write(filename)
 
+
+def csv_writer(filename, data):
+    with open(filename, 'wb') as fp:
+        writer = csv.writer(fp, quoting=csv.QUOTE_ALL)
+        for row in data:
+            writer.writerow(row)
+
+
 # main entrance for script
 class Main:
     def __init__(self):
@@ -95,16 +104,33 @@ class Main:
                       "Clock Time"]
         self.output_path = os.getcwd() + '/' + "output"
         self.items_original = {}
+        self.overview = []
 
     def run(self):
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
 
+        # save to overview file
+        text = ['filename']
+        for i in self.items:
+            config = CONF_DICT[i][0]
+            # if delete, only show original.
+            if config == 1:
+                text.append(i)
+            # if change, show old value and new value.
+            elif config == 2:
+                text.append(i + '_origin')
+                text.append(i + '_new')
+        self.overview.append(text)
+
         for name in glob.glob('*.its'):
             self.filter(name)
 
+        csv_writer(self.output_path + '/' + 'overview.csv', self.overview)
+
     def filter(self, filename):
         parser = XMLParser2(filename)
+        text = [filename]
 
         for i in self.items:
             config = CONF_DICT[i][0]
@@ -114,12 +140,20 @@ class Main:
                 for j in xpaths:
                     self.items_original[i] = parser.get_attr(j[0], j[1])
                     parser.del_attrs(j[0], j[1])
+                    # only put first item in overview
+                    if xpaths.index(j) == 0:
+                        text.append(self.items_original[i])
             # change
             elif config == 2:
                 for j in xpaths:
                     self.items_original[i] = parser.get_attr(j[0], j[1])
                     new_value = self.dummy(i)
                     parser.set_attrs(j[0], j[1], new_value)
+                    # only put first item in overview
+                    if xpaths.index(j) == 0:
+                        text.append(self.items_original[i])
+                        text.append(new_value)
+        self.overview.append(text)
         parser.save(self.output_path + '/' + filename)
 
     def dummy(self, key):
@@ -129,10 +163,12 @@ class Main:
         if key == 'Clock Time':
             # example: 2015-06-10T11:30:20Z
             if self.items_original['Clock Time'][-1] is 'Z':
-                clock_time = datetime.datetime.strptime(self.items_original['Clock Time'], "%Y-%m-%dT%H:%M:%SZ")
+                clock_time = datetime.datetime.strptime(self.items_original['Clock Time'],
+                                                        "%Y-%m-%dT%H:%M:%SZ")
             else:
-                clock_time = datetime.datetime.strptime(self.items_original['Clock Time'], "%Y-%m-%dT%H:%M:%S")
-            
+                clock_time = datetime.datetime.strptime(self.items_original['Clock Time'],
+                                                        "%Y-%m-%dT%H:%M:%S")
+
             dob_time = datetime.datetime.strptime(self.items_original['DOB'], "%Y-%m-%d")
             new_time = datetime.date(2000, 01, 01) + relativedelta(clock_time, dob_time)
 
--- cmd/its_anonymization/readme.md
+++ cmd/its_anonymization/readme.md
@@ -3,26 +3,24 @@ A python script that scans all LENA ITS files in the current folder and remove i
 
 ## Usage:
 Put the main.py in a folder that contains ITS files and run this script.
-It will generate a new folder named "output" in the current location with modified ITS files.
+It will generate a new folder named "output" in the current location with modified ITS files. It will also generate an overview spreadsheet in the output folder named "overview.csv". All changes to ITS files will be showed in the overview. If an item is deleted, it will only show this item. If an item is changed with a dummy value, it will show the original value and new value. 
 
-## Config:
+## Configuration:
 Based on different purposes, this script is designed to be configurable. At the beginning of this script, you will see something like this:
-```python
 CONF_DICT = {
-"Serial Number": [2, "0000"],
-"Gender": [0, -1],
-"Algorithm Age": [0, -1],
-"Child ID": [2, "0000"],
-"Child Key": [2, "0000000000000000"],
-"Enroll Date": [1, -1],
-"DOB": [2, "2000-01-01"],
-"Time Zone": [1, -1],
-"UTC Time": [1, -1],
-"Clock Time": [2, "0"]}
-```
-Each row reprensent one domain item in ITS file. Before colon, it is name of domain item. After colon, it is configuration for this domain item in square bracket.
-
-- 0 stands for no change.
-- 1 stands for delete.
-- 2 stands for use dummy value after comma.
+    "Serial Number": [2, "0000"],
+    "Gender": [0, -1],
+    "Algorithm Age": [0, -1],
+    "Child ID": [2, "0000"],
+    "Child Key": [2, "0000000000000000"],
+    "Enroll Date": [1, -1],
+    "DOB": [2, "2000-01-01"],
+    "Time Zone": [1, -1],
+    "UTC Time": [1, -1],
+    "Clock Time": [2, "0"]
+}
+Before colon, it is domain items in ITS file. After colon, it is configuration for this domain item in square bracket.
+0 stands for no change.
+1 stands for delete.
+2 stands for use dummy value followed after comma.
 
